fc981ac62dad747f199d85c06c6dcf7690620129
ramtejvigna <vignaramtejtelagarapu@gmail.com>
2025-01-26 00:49:03 +0530

    Changed alignment of the feature images

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -596,14 +596,14 @@ const CustomerForm = () => {
 
       {/* Features Section */}
       <div className="max-w-6xl mx-auto px-4 mb-16">
-        <div className="grid md:grid-cols-3 gap-8">
+        <div className={`${features.length < 3 ? `flex flex-col items-center md:flex-row justify-around` : 'grid md:grid-cols-3'} gap-8`}>
           {features.map((feature, index) => (
             <motion.div
               key={feature._id}
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: index * 0.2 }}
-              className="bg-white p-6 rounded-xl shadow-lg hover:shadow-xl transition-shadow"
+              className="bg-white p-6 w-[21rem] rounded-xl shadow-lg hover:shadow-xl transition-shadow"
             >
               <img src={feature.base64} alt={feature.title} className='h-48 w-full rounded-md' />
               <h3 className="text-xl font-semibold my-2 mt-4">{feature.title}</h3>
